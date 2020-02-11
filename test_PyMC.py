4fc9db2564acef6f402f5ffd636d3d3c55b5566d
佐々木　翔 <sasakikakeru@Oyuka-iMac-201704.local>
2020-02-11 17:22:00 +0900

    Chapter8

--- test_PyMC.py
+++ test_PyMC.py
@@ -1,13 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pymc3 as pm
-
-basic_model = pm.Model()
-samples = np.random.binomial(1, 0.5, size=(100000,))
-
-with basic_model:
-    theta = pm.Beta('theta', alpha=1, beta=1)
-    k = pm.Bernoulli('k', p=theta, observed=samples)
-    trace = pm.sample(100000)
-
-pm.plot_posterior(trace[50000:],['theta'])
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('seaborn-darkgrid')
+np.set_printoptions(precision=2)
+pd.set_option('display.precision', 2)
